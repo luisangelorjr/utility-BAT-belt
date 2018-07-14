4e0f6bf00158d7339f19220cb0670f2f20341ccd
Luís Ângelo Rodrigues Jr <luisangelorjr@gmail.com>
2018-07-14 12:52:48 -0300

    change directory for logs

--- InfoPC.cmd
+++ InfoPC.cmd
@@ -2,43 +2,43 @@ echo off
 cls
 rem script em criação para coletar dados
 cls
-md c:\larjr
-wmic /output:c:\larjr\InstalledSoftwareList.txt product get name, version
-wmic /output:c:\larjr\SerialNumber.txt bios get serialnumber
-wmic /output:c:\larjr\UserInLogin.txt computersystem get username
+md c:\larjr\InfoPC
+wmic /output:c:\larjr\InfoPC\InstalledSoftwareList.txt product get name, version
+wmic /output:c:\larjr\InfoPC\SerialNumber.txt bios get serialnumber
+wmic /output:c:\larjr\InfoPC\UserInLogin.txt computersystem get username
 
 rem Obter toda a Informação sobre a CPU
-wmic /output:c:\larjr\InfoCPU.txt CPU get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoCPU.txt CPU get /all /format:LIST
 
 rem Obter toda a Informação sobre as interfaces de REDES
-wmic /output:c:\larjr\InterNetworks.txt NICCONFIG get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InterNetworks.txt NICCONFIG get /all /format:LIST
 
 rem Obter toda a Informação sobre o Sistema
-wmic /output:c:\larjr\InfoSystem.txt COMPUTERSYSTEM get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoSystem.txt COMPUTERSYSTEM get /all /format:LIST
 
 rem Obter toda a Informação sobre os discos.
-wmic /output:c:\larjr\InfoDisks.txt DISKDRIVE get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoDisks.txt DISKDRIVE get /all /format:LIST
 
 rem Obter toda a Informação sobre os discos logicos
-wmic /output:c:\larjr\InfoLogicalDisks.txt LOGICALDISK get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoLogicalDisks.txt LOGICALDISK get /all /format:LIST
 
 rem Obter toda a Informação sobre as memorias.
-wmic /output:c:\larjr\InfoMemory.txt MEMPHYSICAL get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoMemory.txt MEMPHYSICAL get /all /format:LIST
 
 rem Obter toda a Informação sobre os serviços.
-wmic /output:c:\larjr\InfoServices.txt SERVICE get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoServices.txt SERVICE get /all /format:LIST
 
 rem Obter toda a Informação sobre os processos.
-wmic /output:c:\larjr\InfoProcess.txt PROCESS get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoProcess.txt PROCESS get /all /format:LIST
 
 rem Obter toda a Informação sobre os  programas que iniciam junto com Windows.
-wmic /output:c:\larjr\InfoStartupPrograms.txt STARTUP get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoStartupPrograms.txt STARTUP get /all /format:LIST
 
 rem Obter toda a Informação sobre a motherboard.
-wmic /output:c:\larjr\InfoMotherboard.txt ONBOARDDEVICE get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoMotherboard.txt ONBOARDDEVICE get /all /format:LIST
 
 rem Obter toda a Informação sobre o Sistema Operacional.
-wmic /output:c:\larjr\InfoSO.txt RECOVEROS get /all /format:LIST
+wmic /output:c:\larjr\InfoPC\InfoSO.txt RECOVEROS get /all /format:LIST
 
 start c:\larjr
 
