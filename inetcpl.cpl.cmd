153ee44f09b6fd041ea884b1d6160ac2eceba4e0
Luís Ângelo Rodrigues Jr <luisangelorjr@gmail.com>
2018-10-11 14:41:50 -0300

    add managing interface network

--- inetcpl.cpl.cmd
+++ inetcpl.cpl.cmd
@@ -1,8 +1,23 @@
 REM Habilita Proxy
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
+REM reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
 
 REM Desabilita Proxy
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
+REM reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
 
 REM Muda o endereço do proxy
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d proxyserveraddress:proxyport /f
+REM reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d proxyserveraddress:proxyport /f
+
+REM Ver interfaces de rede
+REM netsh interface show interface
+
+REM Exemplo: 
+REM Admin State    State          Type             Interface Name
+REM -------------------------------------------------------------------------
+REM Enabled        Connected      Dedicated        Wi-Fi
+REM Enabled        Disconnected   Dedicated        Ethernet
+
+REM Habilita interface
+REM netsh interface set interface "Wi-Fi" admin=enable
+
+REM Desabilita interface
+REM netsh interface set interface "Wi-Fi" admin=disable
